9792a8d358fbcc3f1f4ddceefa1d93fb24d52575
norepick <norepick@163.com>
2020-04-20 22:56:07 +0800

    CLab6--initial implementation

--- clab/clab6/BubbleGrid.java
+++ clab/clab6/BubbleGrid.java
@@ -1,11 +1,18 @@
 public class BubbleGrid {
     private int[][] grid;
+    private int bNum;
 
     /* Create new BubbleGrid with bubble/space locations specified by grid.
      * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
      * 0's denote a space. */
     public BubbleGrid(int[][] grid) {
         this.grid = grid;
+        bNum = 0;
+        for (int[] row : grid) {
+            for (int b : row) {
+                bNum += b;
+            }
+        }
     }
 
     /* Returns an array whose i-th element is the number of bubbles that
@@ -13,7 +20,107 @@ public class BubbleGrid {
      * are unique, valid locations in the grid. Must be non-destructive
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
-        // TODO
-        return null;
+        int[] result = new int[darts.length];
+        int[] dPos;
+        int drop = 0;
+        int pop = 0;
+        /*for (int i = 0; i < darts.length; i++) {
+            dPos = darts[i];
+            if (grid)
+            UnionFind bubbles = new UnionFind(bNum - pop - drop)
+        }*/
+        // 0th: stuck, 1st: empty, 2nd: a unstuck set, rest are connected unstuck bubbles
+        UnionFind bubbles = new UnionFind(grid.length * grid[0].length + 3);
+        // initialize UnionFind
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && grid[i][j] == 1) { //first row is stuck
+                    bubbles.union(linearInd(i, j), 0);
+                } else if(grid[i][j] == 0) { // empty
+                    bubbles.union(linearInd(i, j), 1);
+                } else if (j == 0) { // first column
+                    if (bubbles.find(linearInd(i - 1, j)) == 0) { // top neighbor is stuck, this one is stuck
+                        bubbles.union(linearInd(i, j), 0);
+                    } else if (bubbles.find(linearInd(i - 1, j)) != 1) {
+                        // top neighbor is unstuck, this one is added to the unstuck union
+                        bubbles.union(linearInd(i, j), linearInd(i - 1, j));
+                    }
+                } else {// rest columns
+                    if (bubbles.find(linearInd(i - 1, j)) == 0 ||
+                            bubbles.find(linearInd(i, j - 1)) == 0) {
+                        // neighbor is stuck, this one and its union is stuck
+                        bubbles.union(linearInd(i, j), 0);
+                    } else {
+                        if (bubbles.find(linearInd(i - 1, j)) != 1) {
+                            // top neighbor is unstuck, this and its union join the unstuck union
+                            bubbles.union(linearInd(i, j), linearInd(i - 1, j));
+                        }
+                        if (bubbles.find(linearInd(i, j - 1)) != 1) {
+                            // left neighbor is unstuck, this and its union join the unstuck union
+                            bubbles.union(linearInd(i, j), linearInd(i, j - 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        int iter = 0;
+        for (int[] d : darts) {
+            // no stuck bubbles pop, next dart
+            if (bubbles.find(linearInd(d[0], d[1])) != 0) {
+                result[iter] = 0;
+                iter += 1;
+                continue;
+            }
+            // a stuck bubble pops, update stuck union
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (bubbles.find(linearInd(i, j)) == 0){
+                        if (i == d[0] && j == d[1]) {
+                            // the chosen bubble is now empty
+                            bubbles.union(linearInd(i, j), 1); // TODO: this will merge the stuck union to empty union
+                        } else {
+                            if (i == 0) { //first row is stuck
+                                continue;
+                            } else if (j == 0) { // first column
+                                if (bubbles.find(linearInd(i - 1, j)) == 0) { // top neighbor is stuck, this one is stuck
+                                    bubbles.union(linearInd(i, j), 0);
+                                } else if (bubbles.find(linearInd(i - 1, j)) != 1) {
+                                    // top neighbor is unstuck, this one is added to the unstuck union
+                                    bubbles.union(linearInd(i, j), linearInd(i - 1, j));
+                                }
+                            } else {// rest columns
+                                if (bubbles.find(linearInd(i - 1, j)) == 0 ||
+                                        bubbles.find(linearInd(i, j - 1)) == 0) {
+                                    // neighbor is stuck, this one and its union is stuck
+                                    bubbles.union(linearInd(i, j), 0);
+                                } else if (bubbles.find(linearInd(i - 1, j)) == 1 &&
+                                        bubbles.find(linearInd(i, j - 1)) == 1) {
+                                    bubbles.union(linearInd(i, j), 2);
+                                }
+                                else {
+                                    if (bubbles.find(linearInd(i - 1, j)) != 1) {
+                                        // top neighbor is unstuck, this and its union join the unstuck union
+                                        bubbles.union(linearInd(i, j), linearInd(i - 1, j));
+                                    }
+                                    if (bubbles.find(linearInd(i, j - 1)) != 1) {
+                                        // left neighbor is unstuck, this and its union join the unstuck union
+                                        bubbles.union(linearInd(i, j), linearInd(i, j - 1));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            result[iter] = bNum + bubbles.sizeOf(0) + 1 - drop;
+            drop = bNum + bubbles.sizeOf(0) + 1;
+            iter += 1;
+        }
+        return result;
+    }
+
+    private int linearInd(int row, int col) {
+        return row * grid[0].length + col + 3;
     }
 }
--- clab/clab6/UnionFind.java
+++ clab/clab6/UnionFind.java
@@ -1,51 +1,76 @@
 public class UnionFind {
-
-    // TODO - Add instance variables?
+    private int[] vertices;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        vertices = new int[n];
+        for (int i = 0; i < n; i++) {
+            vertices[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex >= vertices.length) {
+            throw new RuntimeException("IllegalArgumentException");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        if (vertices[v1] < 0) {
+            return -vertices[v1];
+        } else {
+            return sizeOf(parent(v1));
+        }
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return vertices[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
-    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid
        elements, and a union-by-size heuristic is used. If the sizes of the sets
-       are equal, tie break by connecting v1's root to v2's root. Unioning a 
-       vertex with itself or vertices that are already connected should not 
+       are equal, tie break by connecting v1's root to v2's root. Unioning a
+       vertex with itself or vertices that are already connected should not
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            if (sizeOf(v1) > sizeOf(v2)) {
+                vertices[find(v1)] += vertices[find(v2)]; // add size to v1's root
+                vertices[find(v2)] = find(v1); // connect v2's root to v1's root
+            } else {
+                vertices[find(v2)] += vertices[find(v1)]; // add size to v2's root
+                vertices[find(v1)] = find(v2); // connect v1's root to v2's root
+            }
+        }
+
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        if (parent(vertex) < 0) {
+            return vertex;
+        } else {
+            vertices[vertex] = find(parent(vertex)); // path compression
+            return vertices[vertex];
+        }
     }
 
 }
