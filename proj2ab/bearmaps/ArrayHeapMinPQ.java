a0c181731b82af66081fb283575ec1c0b7174c88
norepick <norepick@163.com>
2020-04-26 17:30:02 +0800

    Proj2a--finished

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,164 @@
+package bearmaps;
+
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Map;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private int size;
+    private Node[] heap;
+    private double loadFactor;
+    private static final int INITSIZE = 4;
+    private static final double LOADLIMIT = 0.75;
+    private Map<T, Integer> elements;
+
+    public ArrayHeapMinPQ() {
+        size = 0;
+        heap = new ArrayHeapMinPQ.Node[INITSIZE];
+        loadFactor = 0;
+        elements = new HashMap<T, Integer>();
+    }
+
+    /* Adds an item with the given priority value. Throws an
+     * IllegalArgumentException if item is already present.
+     * You may assume that item is never null. */
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        }
+        heap[size] = new Node(item, priority);
+        elements.put(item, size);
+        swimUp(size);
+        size += 1;
+        loadFactor = ((double) size) / heap.length;
+        resize();
+    }
+
+    /* Returns true if the PQ contains the given item. */
+    public boolean contains(T item) {
+        return elements.containsKey(item);
+    }
+
+    /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    public T getSmallest() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return heap[0].element;
+    }
+
+    /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
+    public T removeSmallest() {
+        T res = heap[0].element;
+        swap(0, size - 1);
+        size -= 1;
+        loadFactor = ((double) size) / heap.length;
+        sinkDown(0);
+        elements.remove(res);
+        resize();
+        return res;
+    }
+
+    /* Returns the number of items in the PQ. */
+    public int size() {
+        return size;
+    }
+
+    /* Changes the priority of the given item. Throws NoSuchElementException if the item
+     * doesn't exist. */
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        int itemIndex = get(item);
+        heap[itemIndex].priority = priority;
+        if (parent(itemIndex) != -1 && priority < heap[parent(itemIndex)].priority) {
+            swimUp(itemIndex);
+        } else {
+            sinkDown(itemIndex);
+        }
+    }
+
+    private int get(T item) {
+        return elements.get(item);
+    }
+
+    private void swimUp(int i) {
+        int parent = parent(i);
+        if (parent != -1 && heap[i].priority < heap[parent].priority) {
+            swap(i, parent);
+            swimUp(parent);
+        }
+    }
+
+    private void sinkDown(int i) {
+        int lc = lChild(i);
+        int rc = rChild(i);
+
+        if (lc != -1 && rc != -1 && heap[i].priority > heap[lc].priority
+                && heap[i].priority > heap[rc].priority) {
+            swap(i, lc);
+            sinkDown(lc);
+            sinkDown(i); // in case node i's priority is greater than both of its child
+        } else if (lc != -1 && heap[i].priority > heap[lc].priority) {
+            swap(i, lc);
+            sinkDown(lc);
+        } else if (rc != -1 && heap[i].priority > heap[rc].priority) {
+            swap(i, rc);
+            sinkDown(rc);
+        }
+    }
+
+    private void swap(int i, int j) {
+        Node tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+        elements.put(heap[i].element, i);
+        elements.put(heap[j].element, j);
+    }
+
+    private int parent(int i) {
+        if (i == 0) {
+            return -1;
+        } else {
+            return (i - 1) / 2;
+        }
+    }
+
+    private int lChild(int i) {
+        int res = 2 * i + 1;
+        return res < size ? res : -1;
+    }
+
+    private int rChild(int i) {
+        int res = 2 * i + 2;
+        return res < size ? res : -1;
+    }
+
+    private void resize() {
+        if (loadFactor >= LOADLIMIT) {
+            Node[] newHeap = new ArrayHeapMinPQ.Node[heap.length * 2];
+            System.arraycopy(heap, 0, newHeap, 0, size);
+            this.heap = newHeap;
+        } else if (loadFactor < 1 - LOADLIMIT && heap.length > INITSIZE) {
+            Node[] newHeap = new ArrayHeapMinPQ.Node[heap.length / 2];
+            System.arraycopy(heap, 0, newHeap, 0, size);
+            this.heap = newHeap;
+        }
+    }
+
+    private class Node implements Comparable<Node> {
+        T element;
+        double priority;
+
+        Node(T element, double priority) {
+            this.element = element;
+            this.priority = priority;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Double.compare(this.priority, other.priority);
+        }
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,204 @@
 package bearmaps;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void testSanity() {
+        ArrayHeapMinPQ<String> ahpq = new ArrayHeapMinPQ<>();
+    }
+
+    @Test
+    public void testContains() {
+        ArrayHeapMinPQ<String> ahpq = new ArrayHeapMinPQ<>();
+        ahpq.add("a", 0.1);
+        ahpq.add("b", 0.2);
+        ahpq.add("c", 0.3);
+        ahpq.add("d", 0.7);
+        ahpq.add("e", 0.5);
+        assertEquals(5, ahpq.size());
+        assertTrue(ahpq.contains("a"));
+        assertTrue(ahpq.contains("b"));
+        assertTrue(ahpq.contains("c"));
+        assertTrue(ahpq.contains("d"));
+        assertTrue(ahpq.contains("e"));
+    }
+
+    @Test
+    public void testRetrievals() {
+        ArrayHeapMinPQ<String> ahpq = new ArrayHeapMinPQ<>();
+        ahpq.add("a", 0.1);
+        ahpq.add("b", 0.2);
+        ahpq.add("c", 0.3);
+        ahpq.add("d", 0.7);
+        ahpq.add("e", 0.5);
+        assertEquals("a", ahpq.removeSmallest());
+        assertEquals(4, ahpq.size());
+        assertEquals("b", ahpq.getSmallest());
+        assertEquals(4, ahpq.size());
+        assertEquals("b", ahpq.removeSmallest());
+        assertEquals("c", ahpq.removeSmallest());
+        assertEquals("e", ahpq.removeSmallest());
+        assertEquals("d", ahpq.removeSmallest());
+    }
+
+    @Test
+    public void testChange() {
+        ArrayHeapMinPQ<String> ahpq = new ArrayHeapMinPQ<>();
+        ahpq.add("a", 0.1);
+        ahpq.add("b", 0.2);
+        ahpq.add("c", 0.3);
+        ahpq.add("d", 0.7);
+        ahpq.add("e", 0.5);
+        ahpq.changePriority("e", 0);
+        assertEquals("e", ahpq.removeSmallest());
+        assertEquals("a", ahpq.getSmallest());
+        ahpq.changePriority("a", 1);
+        assertEquals("b", ahpq.getSmallest());
+    }
+
+    @Test
+    public void testRandomAdd() {
+        int numOp = 4000000;
+        ArrayHeapMinPQ<Double> ahpq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Double> nmpq = new NaiveMinPQ<>();
+        Random itemGen = new Random(1);
+        Random priorityGen = new Random(2);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            ahpq.add(itemGen.nextDouble(), priorityGen.nextDouble());
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp + " ArrayHeapMinPQ.add operations "
+                + (end - start) / 1000.0 +  " seconds.");
+
+        itemGen = new Random(1);
+        priorityGen = new Random(2);
+        start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            nmpq.add(itemGen.nextDouble(), priorityGen.nextDouble());
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp + " NaiveMinPQ.add operations "
+                + (end - start) / 1000.0 +  " seconds.");
+    }
+
+    @Test
+    public void testRandomContains() {
+        int numOp = 200000;
+        int size = 10000;
+        ArrayHeapMinPQ<Double> ahpq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Double> nmpq = new NaiveMinPQ<>();
+
+        Random itemGen = new Random(1);
+        Random priorityGen = new Random(2);
+        Random indexGen = new Random(3);
+        Double[] items = new Double[size];
+        Double[] priorities = new Double[size];
+        for (int i = 0; i < size; i++) {
+            items[i] = itemGen.nextDouble();
+            priorities[i] = priorityGen.nextDouble();
+            ahpq.add(items[i], priorities[i]);
+            nmpq.add(items[i], priorities[i]);
+        }
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            ahpq.contains(items[indexGen.nextInt(size)]);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " ArrayHeapMinPQ.contains operations "
+                + (end - start) / 1000.0 +  " seconds.");
+
+        indexGen = new Random(3);
+        start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            nmpq.contains(items[indexGen.nextInt(size)]);
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " NaiveMinPQ.contains operations "
+                + (end - start) / 1000.0 +  " seconds.");
+    }
+
+    @Test
+    public void testRandomGetSmallest() {
+        int numOp = 16000;
+        int size = 20000;
+        ArrayHeapMinPQ<Double> ahpq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Double> nmpq = new NaiveMinPQ<>();
+
+        Random itemGen = new Random(1);
+        Random priorityGen = new Random(2);
+        Double[] items = new Double[size];
+        Double[] priorities = new Double[size];
+        for (int i = 0; i < size; i++) {
+            items[i] = itemGen.nextDouble();
+            priorities[i] = priorityGen.nextDouble();
+            ahpq.add(items[i], priorities[i]);
+            nmpq.add(items[i], priorities[i]);
+        }
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            ahpq.getSmallest();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " ArrayHeapMinPQ.getSmallest operations "
+                + (end - start) / 1000.0 +  " seconds.");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            nmpq.getSmallest();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " NaiveMinPQ.getSmallest operations "
+                + (end - start) / 1000.0 +  " seconds.");
+    }
+
+    @Test
+    public void testRandomRemoveSmallest() {
+        int numOp = 8000;
+        int size = 20000;
+        ArrayHeapMinPQ<Double> ahpq = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Double> nmpq = new NaiveMinPQ<>();
+
+        Random itemGen = new Random(1);
+        Random priorityGen = new Random(2);
+        Double[] items = new Double[size];
+        Double[] priorities = new Double[size];
+        for (int i = 0; i < size; i++) {
+            items[i] = itemGen.nextDouble();
+            priorities[i] = priorityGen.nextDouble();
+            ahpq.add(items[i], priorities[i]);
+            nmpq.add(items[i], priorities[i]);
+        }
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            ahpq.removeSmallest();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " ArrayHeapMinPQ.removeSmallest operations "
+                + (end - start) / 1000.0 +  " seconds.");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < numOp; i++) {
+            nmpq.removeSmallest();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Total time elapsed for " + numOp
+                + " NaiveMinPQ.removeSmallest operations "
+                + (end - start) / 1000.0 +  " seconds.");
+    }
 }
