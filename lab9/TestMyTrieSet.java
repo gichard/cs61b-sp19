5c5d9d183f7a39882fcaa8bee4c9fb219444550d
norepick <norepick@163.com>
2020-04-26 22:25:38 +0800

    Lab9--finished

--- lab9/TestMyTrieSet.java
+++ lab9/TestMyTrieSet.java
@@ -54,6 +54,22 @@ public class TestMyTrieSet {
         }
     }
 
+    @Test
+    public void longestPrefixTest() {
+        String[] saStrings = new String[]{"same", "sam", "sad", "sap"};
+        String[] otherStrings = new String[]{"a", "awls", "hello"};
+
+        MyTrieSet t = new MyTrieSet();
+        for (String s: saStrings) {
+            t.add(s);
+        }
+        for (String s: otherStrings) {
+            t.add(s);
+        }
+
+        assertEquals("same", t.longestPrefixOf("sa"));
+    }
+
     public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests(TestMyTrieSet.class);
     }
