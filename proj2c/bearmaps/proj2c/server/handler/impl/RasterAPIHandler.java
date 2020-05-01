b8bf62383039a92bf6fe6e61ea5df00d3daa9b2e
norepick <norepick@163.com>
2020-05-01 20:42:06 +0800

    Proj2c--finished part1

--- proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
+++ proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
@@ -17,8 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static bearmaps.proj2c.utils.Constants.SEMANTIC_STREET_GRAPH;
-import static bearmaps.proj2c.utils.Constants.ROUTE_LIST;
+import static bearmaps.proj2c.utils.Constants.*;
 
 /**
  * Handles requests from the web browser for map images. These images
@@ -84,12 +83,97 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      */
     @Override
     public Map<String, Object> processRequest(Map<String, Double> requestParams, Response response) {
-        //System.out.println("yo, wanna know the parameters given by the web browser? They are:");
-        //System.out.println(requestParams);
+        /*System.out.println("yo, wanna know the parameters given by the web browser? They are:");
+        System.out.println(requestParams);
         Map<String, Object> results = new HashMap<>();
         System.out.println("Since you haven't implemented RasterAPIHandler.processRequest, nothing is displayed in "
-                + "your browser.");
+                + "your browser.");*/
+        Map<String, Object> results = new HashMap<>();
+        boolean query_success = checkCoverage(requestParams);
+        results.put("query_success", query_success);
+        if (query_success) {
+            double lrlon = requestParams.get("lrlon");
+            double ullon = requestParams.get("ullon");
+            double w = requestParams.get("w");
+            double query_ldpp = (lrlon - ullon) / w;
+            int depth = 0;
+            double dth_ldpp = (ROOT_LRLON - ROOT_ULLON) / TILE_SIZE;
+            while (query_ldpp < dth_ldpp && depth < 7) { // max depth is 7
+                depth += 1;
+                dth_ldpp /= 2.0;
+            }
+            results.put("depth", depth);
+
+            results.putAll(findGrid(requestParams, depth));
+        }
+
         return results;
+    }
+
+    private boolean checkCoverage(Map<String, Double> requestParams) {
+        double lrlon = requestParams.get("lrlon");
+        double ullon = requestParams.get("ullon");
+        double lrlat = requestParams.get("lrlat");
+        double ullat = requestParams.get("ullat");
+
+        return !(lrlon <= ROOT_ULLON) && !(ullon >= ROOT_LRLON) // left or right
+                && !(lrlat >= ROOT_ULLAT) && !(ullat <= ROOT_LRLAT); // up or down
+    }
+
+    private Map<String, Object> findGrid(Map<String, Double> requestParams, int depth) {
+        double lrlon = requestParams.get("lrlon");
+        double ullon = requestParams.get("ullon");
+        double lrlat = requestParams.get("lrlat");
+        double ullat = requestParams.get("ullat");
+        Map<String, Object> results = new HashMap<>();
+
+        int[] xTiles = gridHelper(ullon, lrlon, true, depth);
+        int[] yTiles = gridHelper(ullat, lrlat, false, depth);
+        String[][] render_grid = new String[yTiles.length][xTiles.length];
+        for (int r = 0; r < yTiles.length; r++) {
+            for (int c = 0; c < xTiles.length; c++) {
+                render_grid[r][c] = String.format("d%d_x%d_y%d.png", depth, xTiles[c], yTiles[r]);
+            }
+        }
+        results.put("render_grid", render_grid);
+
+        results.put("raster_ul_lon", xTiles[0] * (ROOT_LRLON - ROOT_ULLON) / Math.pow(2, depth)
+                + ROOT_ULLON);
+        results.put("raster_ul_lat", yTiles[0] * (ROOT_LRLAT - ROOT_ULLAT) / Math.pow(2, depth)
+                + ROOT_ULLAT);
+        results.put("raster_lr_lon", (xTiles[xTiles.length - 1] + 1) * (ROOT_LRLON - ROOT_ULLON)
+                / Math.pow(2, depth) + ROOT_ULLON);
+        results.put("raster_lr_lat", (yTiles[yTiles.length - 1] + 1) * (ROOT_LRLAT - ROOT_ULLAT)
+                / Math.pow(2, depth) + ROOT_ULLAT);
+
+        return results;
+    }
+
+    private int[] gridHelper(double start, double end, boolean isX, int depth) {
+        int size = (int) Math.pow(2, depth);
+        int[] inds = new int[2];
+        if (isX) {
+            inds[0] = start < ROOT_ULLON ? 0
+                    : (int) Math.floor((start - ROOT_ULLON)
+                    / (ROOT_LRLON - ROOT_ULLON) * size);
+            inds[1] = end > ROOT_LRLON ? size
+                    : (int) Math.ceil((end - ROOT_ULLON)
+                    / (ROOT_LRLON - ROOT_ULLON) * size);
+        } else {
+            inds[0] = start > ROOT_ULLAT ? 0
+                    : (int) Math.floor((ROOT_ULLAT - start)
+                    / (ROOT_ULLAT - ROOT_LRLAT) * size);
+            inds[1] = end < ROOT_LRLAT ? size
+                    : (int) Math.ceil((ROOT_ULLAT - end)
+                    / (ROOT_ULLAT - ROOT_LRLAT) * size);
+        }
+
+        int[] res = new int[inds[1] - inds[0]];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = inds[0] + i;
+        }
+
+        return res;
     }
 
     @Override
